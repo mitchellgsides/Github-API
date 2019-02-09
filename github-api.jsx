2df6dad339db69aabbd975554d1a5d3c687e52c0
mitchellgsides <mitchellgsides@gmail.com>
2019-02-08 21:28:46 -0600

    github api initial commit

---
+++ github-api.jsx
@@ -0,0 +1,38 @@
+'use strict';
+
+function getResults() {
+    fetch(`https://api.github.com/users/${$('#search-terms').val()}/repos`)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('invalid username'));
+    
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#results-listing').empty();
+    //log all search results
+    for(let i = 0; i < responseJson.length; i++) {
+    console.log(responseJson[i].name);
+    console.log(responseJson[i].html_url)
+    }
+    //place search into html
+    for(let i = 0; i < responseJson.length; i++) {
+        $('#results-listing').append(`<li><a href='${responseJson[i].html_url}'>${responseJson[i].name}</a></li>`)
+    }
+    $('#github-username').text($('#search-terms').val().toLowerCase());
+
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        getResults();
+        console.log();
+    })
+}
+
+$(function() {
+    console.log('App loaded! Waiting for submit!')
+    watchForm();
+});
